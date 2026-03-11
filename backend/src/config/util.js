23a58358fc212726eadfac9e8065ee78250b1236
Kiran5258 <skirankumar97849@gmail.com>
2026-03-11 20:23:51 +0530

    now deploy

--- backend/src/config/util.js
+++ backend/src/config/util.js
@@ -6,8 +6,8 @@ const generateToken = (userId, res) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    secure: true, // Required for sameSite: 'none'
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
